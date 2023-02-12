51908ea6216f662b344e7b50d5848f05299bffe3
Manuu <manutoledo231@gmail.com>
2023-02-12 19:36:24 -0300

    indexPage: add grid & title comp

--- src/pages/PageDetails.jsx
+++ src/pages/PageDetails.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CharacterDetails from '../components/CharacterDetails';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Title from '../components/Title';
 
 const PageIndex = () => {
   return (
